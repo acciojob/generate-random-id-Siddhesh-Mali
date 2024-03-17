bd3b52e0e78293a8e26711e7ee5a5b8942c5964f
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-18 01:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function makeid(l) {
-  // write your code here
-}
+
+function createLetterAndDigitArray() {
+    const letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65)); // A-Z
+    letters.push(...Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97))); // a-z
+    const digits = Array.from({ length: 10 }, (_, i) => String(i)); // 0-9
+  
+    return letters.concat(digits); 
+  }
+  
+  function makeid(l) {
+    const chars = createLetterAndDigitArray(); 
+        let result = '';
+  
+    for (let i = 0; i < l; i++) {
+      const index = Math.floor(Math.random() * chars.length);
+      result += chars[index];
+    }
+  
+    return result;
+  }
+  
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
